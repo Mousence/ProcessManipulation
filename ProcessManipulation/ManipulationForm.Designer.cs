d1020c3508783180ac5a811a184b2604a936f651
79141 <79141@DESKTOP-GMGD17F>
2024-05-18 21:41:06 +0900

    Add project files.

---
+++ ProcessManipulation/ManipulationForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ProcessManipulation
+{
+	partial class ManipulationForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnStart = new System.Windows.Forms.Button();
+			this.listBoxRunnedAssemblies = new System.Windows.Forms.ListBox();
+			this.listBoxAvalableAssemblies = new System.Windows.Forms.ListBox();
+			this.lblAvalableAssembles = new System.Windows.Forms.Label();
+			this.lblRunnedAssembles = new System.Windows.Forms.Label();
+			this.btnCloseWindow = new System.Windows.Forms.Button();
+			this.btnRefresh = new System.Windows.Forms.Button();
+			this.btnStop = new System.Windows.Forms.Button();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.SuspendLayout();
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(367, 36);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(75, 23);
+			this.btnStart.TabIndex = 2;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// listBoxRunnedAssemblies
+			// 
+			this.listBoxRunnedAssemblies.FormattingEnabled = true;
+			this.listBoxRunnedAssemblies.Location = new System.Drawing.Point(467, 36);
+			this.listBoxRunnedAssemblies.Name = "listBoxRunnedAssemblies";
+			this.listBoxRunnedAssemblies.Size = new System.Drawing.Size(322, 225);
+			this.listBoxRunnedAssemblies.TabIndex = 3;
+			// 
+			// listBoxAvalableAssemblies
+			// 
+			this.listBoxAvalableAssemblies.FormattingEnabled = true;
+			this.listBoxAvalableAssemblies.Location = new System.Drawing.Point(12, 36);
+			this.listBoxAvalableAssemblies.Name = "listBoxAvalableAssemblies";
+			this.listBoxAvalableAssemblies.Size = new System.Drawing.Size(322, 225);
+			this.listBoxAvalableAssemblies.TabIndex = 4;
+			// 
+			// lblAvalableAssembles
+			// 
+			this.lblAvalableAssembles.AutoSize = true;
+			this.lblAvalableAssembles.Location = new System.Drawing.Point(12, 9);
+			this.lblAvalableAssembles.Name = "lblAvalableAssembles";
+			this.lblAvalableAssembles.Size = new System.Drawing.Size(106, 13);
+			this.lblAvalableAssembles.TabIndex = 5;
+			this.lblAvalableAssembles.Text = "Доступные сборки:";
+			// 
+			// lblRunnedAssembles
+			// 
+			this.lblRunnedAssembles.AutoSize = true;
+			this.lblRunnedAssembles.Location = new System.Drawing.Point(464, 9);
+			this.lblRunnedAssembles.Name = "lblRunnedAssembles";
+			this.lblRunnedAssembles.Size = new System.Drawing.Size(114, 13);
+			this.lblRunnedAssembles.TabIndex = 6;
+			this.lblRunnedAssembles.Text = "Запущенные сборки:";
+			// 
+			// btnCloseWindow
+			// 
+			this.btnCloseWindow.Location = new System.Drawing.Point(367, 94);
+			this.btnCloseWindow.Name = "btnCloseWindow";
+			this.btnCloseWindow.Size = new System.Drawing.Size(75, 23);
+			this.btnCloseWindow.TabIndex = 7;
+			this.btnCloseWindow.Text = "CloseWindow";
+			this.btnCloseWindow.UseVisualStyleBackColor = true;
+			// 
+			// btnRefresh
+			// 
+			this.btnRefresh.Location = new System.Drawing.Point(367, 123);
+			this.btnRefresh.Name = "btnRefresh";
+			this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+			this.btnRefresh.TabIndex = 8;
+			this.btnRefresh.Text = "Refresh";
+			this.btnRefresh.UseVisualStyleBackColor = true;
+			// 
+			// btnStop
+			// 
+			this.btnStop.Location = new System.Drawing.Point(367, 65);
+			this.btnStop.Name = "btnStop";
+			this.btnStop.Size = new System.Drawing.Size(75, 23);
+			this.btnStop.TabIndex = 9;
+			this.btnStop.Text = "Stop";
+			this.btnStop.UseVisualStyleBackColor = true;
+			this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Location = new System.Drawing.Point(367, 153);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowse.TabIndex = 10;
+			this.btnBrowse.Text = "Browse";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// ManipulationForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.btnStop);
+			this.Controls.Add(this.btnRefresh);
+			this.Controls.Add(this.btnCloseWindow);
+			this.Controls.Add(this.lblRunnedAssembles);
+			this.Controls.Add(this.lblAvalableAssembles);
+			this.Controls.Add(this.listBoxAvalableAssemblies);
+			this.Controls.Add(this.listBoxRunnedAssemblies);
+			this.Controls.Add(this.btnStart);
+			this.Name = "ManipulationForm";
+			this.Text = "Manipulator";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ManipulationForm_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.ListBox listBoxRunnedAssemblies;
+		private System.Windows.Forms.ListBox listBoxAvalableAssemblies;
+		private System.Windows.Forms.Label lblAvalableAssembles;
+		private System.Windows.Forms.Label lblRunnedAssembles;
+		private System.Windows.Forms.Button btnCloseWindow;
+		private System.Windows.Forms.Button btnRefresh;
+		private System.Windows.Forms.Button btnStop;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+	}
+}
+
---
+++ ProcessManipulation/ManipulationForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+using System.Management;
+
+namespace ProcessManipulation
+{
+	public partial class ManipulationForm : Form
+	{
+		List<Process> processes = new List<Process>();
+		int counter = 0;
+		delegate void ProcessDelegate(Process process);
+		public ManipulationForm()
+		{
+			InitializeComponent();
+			LoadAvailableAssemblies();
+		}
+		private void process_Exited(object sender, EventArgs e)
+		{
+			if (InvokeRequired) {
+				Invoke(new Action<object, EventArgs>(process_Exited), sender, e);
+				return;
+			}
+
+			Process process = sender as Process;
+			listBoxRunnedAssemblies.Items.Remove(process.ProcessName);
+			listBoxAvalableAssemblies.Items.Add(process.ProcessName);
+			processes.Remove(process);
+			counter--;
+
+			for (int i = 0; i < processes.Count; i++)
+				SetChildWindowText(processes[i].MainWindowHandle, $"Child process {i + 1}");
+		}
+		void LoadAvailableAssemblies(string FilePath = null)
+		{
+			string except = new FileInfo(Application.ExecutablePath).Name.Split('.').First();
+			string[] files;
+			if (FilePath == null) files = Directory.GetFiles(Application.StartupPath, "*.exe");
+			else files = Directory.GetFiles(FilePath, "*.exe");
+
+			for (int i = 0; i < files.Length; i++)
+				listBoxAvalableAssemblies.Items.Add(files[i].Split('\\').Last().Split('.').First());
+			
+			listBoxAvalableAssemblies.Items.Remove(except);
+		}
+		void RunProcess(string assemblyName)
+		{
+			Process process = Process.Start(folderBrowserDialog1.SelectedPath+'\\'+assemblyName);
+			processes.Add(process);
+
+			process.EnableRaisingEvents = true;
+			process.Exited += process_Exited;
+			SendMessage(process.MainWindowHandle, WM_SETTEXT, (System.IntPtr)0, $"Child process #{processes.Count}");
+			listBoxRunnedAssemblies.Items.Add(process.ProcessName);
+			listBoxAvalableAssemblies.Items.Remove(process.ProcessName);
+		}
+		void ExecuteOnProcessByName(string name, ProcessDelegate func)
+		{
+			Process[] processes = Process.GetProcessesByName(name);
+			if (processes.Length == 0)
+			{
+				MessageBox.Show($"No running process found with the name {name}.");
+				return;
+			}
+
+			foreach (Process proc in processes)
+				if (Process.GetCurrentProcess().Id == GetParentProcessId(proc.Id)) func(proc);
+		}
+		int GetParentProcessId(int id)
+		{
+			int parentID;
+			using (ManagementObject obj = new ManagementObject("win32_process.handle=" + id.ToString()))
+			{
+				obj.Get();
+				parentID = Convert.ToInt32(obj["ParentProcessId"]);
+			}
+			return parentID;
+		}
+		void SetChildWindowText(IntPtr handle, string text)
+		{
+			SendMessage(handle, WM_SETTEXT, (System.IntPtr)0, text);
+		}
+		/// <summary>
+		/// API function:
+		/// </summary>
+		const uint WM_SETTEXT = 0x0C;
+		[DllImport("user32.dll")]
+		public static extern IntPtr SendMessage(IntPtr hwnd, uint uMsg, IntPtr wParam,
+			[MarshalAs(UnmanagedType.LPStr)] string lParam);
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			if (listBoxAvalableAssemblies.SelectedItem != null)
+				RunProcess(listBoxAvalableAssemblies.SelectedItem.ToString());
+		}
+
+		private void ManipulationForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			foreach (Process proc in processes)
+			{
+				proc.CloseMainWindow();
+				proc.Close();
+			}
+		}
+		void Kill(Process process)
+		{
+			process.Kill();
+		}
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			if (listBoxRunnedAssemblies.SelectedItem != null)
+			{
+				string selectedProcessName = listBoxRunnedAssemblies.SelectedItem.ToString();
+				ExecuteOnProcessByName(selectedProcessName, Kill);
+				listBoxRunnedAssemblies.Items.Remove(selectedProcessName);
+			}
+			else
+				MessageBox.Show("Select a process to stop.");
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			folderBrowserDialog1.SelectedPath = Application.StartupPath;
+			if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				listBoxAvalableAssemblies.Items.Clear();
+				LoadAvailableAssemblies(folderBrowserDialog1.SelectedPath);
+				//MessageBox.Show(this, folderBrowserDialog1.SelectedPath, "Selected folder: ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+	}
+}
